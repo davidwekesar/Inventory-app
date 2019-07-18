a3be944179035c53d6c147c8b1e8c53473a401e7
davidwekesar <davidwekesar@gmail.com>
2019-07-18 20:55:36 +0300

    inventory database helper

--- app/src/main/java/com/example/inventoryapp/data/InventoryDbHelper.java
+++ app/src/main/java/com/example/inventoryapp/data/InventoryDbHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.inventoryapp.data.InventoryContract.InventoryEntry;
+
 public class InventoryDbHelper extends SQLiteOpenHelper {
     // Name of the database file
     public static final String DATABASE_NAME = "inventory.db";
@@ -16,11 +18,22 @@ public class InventoryDbHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String SQL_CREATE_INVENTORY_TABLE = "CREATE TABLE " + InventoryEntry.TABLE_NAME + "("
+                + InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + InventoryEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL,"
+                + InventoryEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL,"
+                + InventoryEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL,"
+                + InventoryEntry.COLUMN_PRODUCT_SUPPLIER + " TEXT NOT NULL,"
+                + InventoryEntry.COLUMN_PRODUCT_IMAGE + " IMAGE NOT NULL);";
+        db.execSQL(SQL_CREATE_INVENTORY_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
     }
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + InventoryEntry.TABLE_NAME;
 }
