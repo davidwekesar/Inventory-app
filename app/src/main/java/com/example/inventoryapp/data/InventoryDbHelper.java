3bb7a4c218466e7de655634fa5e9faaccda37c96
davidwekesar <davidwekesar@gmail.com>
2019-07-18 18:20:53 +0300

    Created a database helper named InventoryDbHelper

--- app/src/main/java/com/example/inventoryapp/data/InventoryDbHelper.java
+++ app/src/main/java/com/example/inventoryapp/data/InventoryDbHelper.java
@@ -1,4 +1,26 @@
 package com.example.inventoryapp.data;
 
-public class Inventorydbhelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class InventoryDbHelper extends SQLiteOpenHelper {
+    // Name of the database file
+    public static final String DATABASE_NAME = "inventory.db";
+
+    // Database version
+    public static final int DATABASE_VERSION = 1;
+
+    public InventoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
