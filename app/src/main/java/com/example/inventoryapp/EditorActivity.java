c0bf33ea25623edb164c99297879fa329079ee09
davidwekesar <davidwekesar@gmail.com>
2019-07-26 21:09:35 +0300

    Added a ContentProvider

--- app/src/main/java/com/example/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/inventoryapp/EditorActivity.java
@@ -2,11 +2,39 @@ package com.example.inventoryapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class EditorActivity extends AppCompatActivity {
+
+    /**
+     * EditText field to enter the product's name
+     */
+    private EditText mNameEditText;
+
+    /**
+     * EditText field to enter the product's price
+     */
+    private EditText mPriceEditText;
+
+    /**
+     * EditText field to enter the product's quantity
+     */
+    private EditText mQuantityEditText;
+
+    /**
+     * EditText field to enter the product's supplier
+     */
+    private EditText mSupplierEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        mNameEditText = findViewById(R.id.edit_product_name);
+        mPriceEditText = findViewById(R.id.edit_product_price);
+        mQuantityEditText = findViewById(R.id.edit_product_quantity);
+        mSupplierEditText = findViewById(R.id.edit_product_supplier);
     }
 }
--- app/src/main/java/com/example/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/example/inventoryapp/data/InventoryContract.java
@@ -1,5 +1,7 @@
 package com.example.inventoryapp.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -12,10 +14,44 @@ public final class InventoryContract {
     }
 
     /**
+     * Use the package name of the app for the content authority, which is guaranteed to be unique
+     * on the device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.inventoryapp";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     */
+    public static final String PATH_INVENTORY = "inventory";
+
+    /**
      * Inner class that defines constant values for the inventory database table.
      * Each entry in the table represents a single product.
      */
     public static final class InventoryEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the inventory data in the provider.
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products or inventory
+         */
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single product
+         */
+        public static  final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE +
+                "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
 
         /**
          * Name of the database table for inventory
